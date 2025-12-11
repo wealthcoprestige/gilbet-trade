9f51aad7d811c6042a625ac6a5df1ed13bc88213
coded <coded@gmail.com>
2025-12-11 18:10:23 +0000

    init

---
+++ src/app/deposite/page.tsx
@@ -0,0 +1,12 @@
+import Deposit from '@/components/LandingPage/Deposite'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <Deposit />
+    </div>
+  )
+}
+
+export default page
